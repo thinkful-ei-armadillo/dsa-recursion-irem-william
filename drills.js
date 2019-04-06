18ad1638052e3e0ba0665b9069bf4b2fde5807ef
Irem Sen <secilreel@yahoo.com>
2019-04-06 12:22:33 -0400

    finished other drills

--- drills.js
+++ drills.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // 1. Counting Sheep
 function countingSheep(num) {
   if (num === 0) {
-    console.log('All sheep jumped over the fence');
+    console.log("All sheep jumped over the fence");
     return;
   }
   //base case 0 stop return
@@ -17,13 +17,12 @@ function countingSheep(num) {
 
 // countingSheep(3);
 
-
 // 2. Power Calculator
 function powerCalculator(base, exp) {
   //base case when exponent = 0
   //cause then it will just return base
   if (exp < 0) {
-    return 'exponent should be >= 0';
+    return "exponent should be >= 0";
   } else if (exp === 0) {
     return 1;
   } else {
@@ -33,21 +32,19 @@ function powerCalculator(base, exp) {
 
 // console.log(powerCalculator(10, -2));
 
-
 // 3. Reverse String
 
 function reverseString(string) {
   // input: string
   // output: string but reversed
   if (string.length === 0) {
-    return '';
+    return "";
   } else {
     return string[string.length - 1] + reverseString(string.slice(0, -1));
   }
 }
 
 // console.log(reverseString(''));
-
 
 // 4. nth Triangular Number
 
@@ -63,42 +60,38 @@ function triangular(n) {
 
 // console.log(triangular(1));
 
-
 // 5. String Splitter
 
 function splitter(string, separator) {
   // input: string, separator 02/20/2000
   // output: string without separator
   if (string.length === 0) {
-    return '';
+    return "";
   } else if (string[0] === separator) {
     return splitter(string.slice(1), separator);
   } else {
     return string[0] + splitter(string.slice(1), separator);
   }
-} 
+}
 
 // console.log(splitter('02/20/2000', '/'));
 
 // 6. Fibonacci
 
-function fib(num){
+function fib(num) {
   //base case 1
   //add fib(n-2)+ fib(n-1)
-  if(num === 0){
+  if (num === 0) {
     return;
-  }
-  else if (num === 1){
+  } else if (num === 1) {
+    return 1;
+  } else if (num === 2) {
     return 1;
   }
-  else if (num === 2){
-    return 1;
-  }
-  return fib(num-1)+fib(num-2);
+  return fib(num - 1) + fib(num - 2);
 }
 
 // console.log(fib(7));
-
 
 // 7. Factorial
 
@@ -113,21 +106,20 @@ function factorial(n) {
 
 // console.log(factorial(5));
 
-
 // 8. Find a way out of the maze
 
 let maze = [
-  [' ', ' ', ' ', '*', ' ', ' ', ' '],
-  ['*', '*', ' ', '*', ' ', '*', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', '*', '*', '*', '*', '*', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+  [" ", " ", " ", "*", " ", " ", " "],
+  ["*", "*", " ", "*", " ", "*", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", "*", "*", "*", "*", "*", " "],
+  [" ", " ", " ", " ", " ", " ", "e"]
 ];
 
 // column & rows
 
 // let steps = '';
-// traverse(column, row); 
+// traverse(column, row);
 // if(maze[column][row] === 'exit')
 //   return;
 // if ([column][row] === ' ') {
@@ -138,50 +130,122 @@ let maze = [
 //   traverse(column, row + 1);
 // }
 
-
 function exitMaze(maze) {
   // input: maze
   // output: RRDDLLDDRRRRRR
-//   if (maze[0] === 'e') {
-//     return '';
-//   } else if (maze[0] === ' ') {
-//     return 'R' + exitMaze(maze.slice(1));
-//   } else if (maze[0] === '*') {
-//     return 'D' || 'R' + exitMaze(maze.slice(1));
-//   } else {
-//     return 'D';
-//   }
+  //   if (maze[0] === 'e') {
+  //     return '';
+  //   } else if (maze[0] === ' ') {
+  //     return 'R' + exitMaze(maze.slice(1));
+  //   } else if (maze[0] === '*') {
+  //     return 'D' || 'R' + exitMaze(maze.slice(1));
+  //   } else {
+  //     return 'D';
+  //   }
 }
 
 // console.log(exitMaze(maze));
 
-
 // 10. Anagrams
+// input: string ('east')
+// output: list of strings (24 strings)
 
 function anagrams(prefix, word) {
-  // input: string ('east')
-  // output: list of strings (24 strings)
-  // let result = [];
   // base case
-  if (word === '') 
-    return [prefix];
-  let words = [];
-  for (let i = 0; i < word.length; i++) {
-    words = [...anagrams(prefix + word[i], word.slice(0, i) + word.slice(i+1))];
+  if (word.length <= 1) {
+    console.log(`${prefix}${word}`);
+  } else {
+    for (let i = 0; i < word.length; i++) {
+      anagrams(prefix + word[i], word.slice(0, i) + word.slice(i + 1));
+    }
   }
-  return words;
-  // console.log(word.slice[-1]);
-  // return anagram(word.slice[-1]) + word.slice(0,1);
 }
-console.log(anagrams('', 'cat'));
+// console.log(anagrams(" ", "east"));
 
+// 11. Organization Chart
+// worker: {}
 
-// for (let i = 0; i < word.length; i++) {
-//   let currentChar = word.substring(i, i+1);
-//   // console.log(currentChar);
-//   let prevChar = word.substring(0, i);
-//   // console.log(prevChar);
-//   let nextChar = word.substring(i + 1);
-//   // console.log(nextChar);
-//   anagram(prefix + currentChar, prevChar + nextChar);
+function organizationChart(obj, indent = 0) {
+  Object.keys(obj).forEach(key => {
+    console.log(" ".repeat(indent), key);
+    organizationChart(obj[key], indent + 4);
+  });
+}
 
+let organization = {
+  Zuckerberg: {
+    Schroepfer: {
+      Bosworth: {
+        Steve: {},
+        Kyle: {},
+        Andra: {}
+      },
+      Zhao: {
+        Richie: {},
+        Sofia: {},
+        Jen: {}
+      },
+      Badros: {
+        John: {},
+        Mike: {},
+        Pat: {}
+      },
+      Parikh: {
+        Zach: {},
+        Ryan: {},
+        Tes: {}
+      }
+    },
+    Schrage: {
+      VanDyck: {
+        Sabrina: {},
+        Michelle: {},
+        Josh: {}
+      },
+      Swain: {
+        Blanch: {},
+        Tom: {},
+        Joe: {}
+      },
+      Frankovsky: {
+        Jasee: {},
+        Brian: {},
+        Margaret: {}
+      }
+    },
+    Sandberg: {
+      Goler: {
+        Eddie: {},
+        Julie: {},
+        Annie: {}
+      },
+      Hernandez: {
+        Rowi: {},
+        Inga: {},
+        Morgan: {}
+      },
+      Moissinac: {
+        Amy: {},
+        Chuck: {},
+        Vinni: {}
+      },
+      Kelley: {
+        Eric: {},
+        Ana: {},
+        Wes: {}
+      }
+    }
+  }
+};
+// console.log(organizationChart(organization, 1));
+
+// 12. Binary Representation
+
+function binary(num) {
+  if (num <= 0) return('');
+  let binaryRep = (num%2);
+  return binary(Math.floor(num/2))+binaryRep;
+
+}
+
+console.log(binary(5));
